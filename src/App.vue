50a564ad0fdfa3526b570c7dc3e685ee637d0ee1
pratikravindramane <p.mane7727@gmail.com>
2025-01-27 23:14:15 +0530

    GET, UPDATE, CREATE, with vue with Setup

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,26 @@
-<script>
-export default {
-  data() {
-    return {
-      name: 'Pratik Mane',
-      status: true,
-      tasks: ['Task One', 'Task Two', 'Task Three'],
-    }
-  },
-  methods: {
-    toggle() {
-      this.status = !this.status
-      console.log(this.status)
-    },
-  },
+<script setup>
+import { ref } from 'vue'
+const name = ref('Pratik Mane')
+const status = ref(true)
+const newTask = ref('')
+let tasks = ref(['Task One', 'Task Two', 'Task Three'])
+const toggle = () => {
+  this.status = !this.status
+  console.log(this.status)
+}
+
+const addTask = () => {
+  if (newTask.value.trim() == '') return
+  tasks.value.push(newTask.value)
+  newTask.value = ''
 }
 </script>
 <template>
   <h1>{{ name }}</h1>
   <p v-if="status">User is Active</p>
   <p v-else>User is Inactive</p>
+  <input type="text" name="tasks" id="tasks" v-model="newTask" />
+  <button @click="addTask">submit</button>
   <h3>Tasks:</h3>
   <ul>
     <li v-for="task in tasks" :key="task">

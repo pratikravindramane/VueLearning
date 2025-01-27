b115ea836601c9e8ee7e558fd8ec7670159f9a8e
pratikravindramane <p.mane7727@gmail.com>
2025-01-27 22:52:04 +0530

    GET, UPDATE, CREATE, with vue

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  data() {
+    return {
+      name: 'Pratik Mane',
+      status: true,
+      tasks: ['Task One', 'Task Two', 'Task Three'],
+    }
+  },
+  methods: {
+    toggle() {
+      this.status = !this.status
+      console.log(this.status)
+    },
+  },
+}
+</script>
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status">User is Active</p>
+  <p v-else>User is Inactive</p>
+  <h3>Tasks:</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">
+      {{ task }}
+    </li>
+  </ul>
+
+  <button v-on:click="toggle">Toggle Status</button>
+</template>
+
+<style scoped></style>
